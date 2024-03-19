0fcd88cf6790190f4befe00397c8fb53236bfcc4
Sumanyu301 <sumanyukodan301@gmail.com>
2024-03-19 16:57:56 +0530

    Add header, body, and layout components

--- app.js
+++ app.js
@@ -1,5 +1,63 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("h1", {}, "hello from react"); //core thing from react
-const root = ReactDOM.createRoot(document.getElementById("root")); //dom
-root.render(heading);
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://static.vecteezy.com/system/resources/previews/011/402/817/original/call-food-logo-design-food-delivery-service-logo-concept-free-vector.jpg"
+        />
+      </div>
+      <div className="nav-item">
+        <ul>
+          <li>home</li>
+          <li>about us</li>
+          <li>contact us</li>
+          <li>cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCards = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="img-card"
+        src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574"
+      ></img>
+      <h3>landhour bakehouse</h3>
+      <h4>asian,north-indian,</h4>
+      <h4>asian,north-indian,</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="restaurant-container">
+        <RestaurantCards />
+        <RestaurantCards />
+        <RestaurantCards />
+        <RestaurantCards />
+        <RestaurantCards />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header></Header>
+      <Body></Body>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
